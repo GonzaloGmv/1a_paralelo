d8bf585581aa3750a1efc3451c3e8773fcf71c21
GonzaloGmv <91721237+GonzaloGmv@users.noreply.github.com>
2023-02-20 15:54:24 +0100

    hechos

--- multiprocesamiento.py
+++ multiprocesamiento.py
@@ -1,9 +1,10 @@
-import random
+from multiprocessing import Pool
 from time import sleep
+import random
 
 def scrape(url):
     print("starting", url)
-    duration = round(random.random(), 3)
+    duration = round(random.random(),3)
     sleep(duration)
     print("finished", url, "time taken:", duration, "seconds")
     return url, duration
@@ -11,7 +12,9 @@ def scrape(url):
 urls = ["a.com", "b.com", "c.com", "d.com"]
 
 if __name__ == "__main__":
-    output = []
-    for url in urls:
-        result = scrape(url)
-        output.append(result)
+    pool = Pool(processes=4)
+    data = pool.map(scrape, urls)
+    pool.close()    
+    print()
+    for row in data:
+        print(row)
